436e17126b3b485e176600dcb8256ced85bc0a82
Chelsea Trotter <chelsea.xhu@gmail.com>
2020-11-16 10:20:07 -0600

    renaming some files.

--- MyApp/precompile_app.jl
+++ MyApp/precompile_app.jl
@@ -1,11 +1,11 @@
 using MyApp
 
 println(@__DIR__)
-genoprobfile=joinpath(@__DIR__, "..", "data", "processed", "spleen-bxd-genoprob.csv")
-gmapfile=joinpath(@__DIR__,"..", "data", "processed", "BXD_gmap.csv")
-cleanphenofile=joinpath(@__DIR__,"..", "data", "processed", "spleen-pheno-nomissing.csv")
+genoprobfile=joinpath(@__DIR__, "..", "data", "spleen-bxd-genoprob.csv")
+gmapfile=joinpath(@__DIR__,"..", "data", "BXD_gmap.csv")
+cleanphenofile=joinpath(@__DIR__,"..", "data", "spleen-pheno-nomissing.csv")
 export_matrix="false"
-output_file=joinpath(@__DIR__,"..", "data", "processed", "julia_result.csv")
+output_file=joinpath(@__DIR__,"..", "data", "julia_result.csv")
 
 push!(ARGS, genoprobfile)
 push!(ARGS, cleanphenofile)
--- install_packages.jl
+++ install_packages.jl
@@ -1,9 +1,9 @@
 using Pkg
 
-Pkg.add(url="https://github.com/senresearch/LMGPU.jl")
+Pkg.add(url="https://github.com/chelseatrotter/LMGPU.jl")
 
-Pkg.activate(".")
-Pkg.instantiate(; verbose = false)
+# Pkg.activate(".")
+# Pkg.instantiate(; verbose = false)
 Pkg.activate("./bin/MyApp")
 Pkg.instantiate(; verbose = false)
 
